8a1bcde854c6aa2bfac86082ed14ba95952d7ce8
alexBob <bboybrelock@gmail.com>
2021-03-04 19:23:38 +0200

    .

--- src/js/index.js
+++ src/js/index.js
@@ -20,49 +20,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 		const win = window,
 			  dom = document,
-			  body = document.body,
-				app = {}
-				// обнуление таблицы
-				$clearFormBtns = $('.forms-button-another'),
+			  body = document.body
+			
+		const fn = {
+					
+			hexlet() {
 				
 
-			  name = document.getElementsByClassName("mainWrapper")
-
-		
-	
-		const fn = {
-
-      // реверс строки 
-      recString(str){
-        let newStr = '', i;
-        for (i = str.length - 1; i >= 0; i--) {
-         newStr += str.charAt(i);
-        }
-          return newStr;
-      },
-      
-			// обнуление таблицы
-			handleClearForm() {
-				$clearFormBtns.on('click', function() {
-					const $this = $(this);
-					const $form = $this.closest('form');
-					const $findBtnActive = $form.find(".button-calculate")
-					$findBtnActive.removeClass("active")
-					fn.resetForm($form);
-				});
-			},
-			resetForm(formElement) {
-				const form = formElement instanceof jQuery ? formElement[0] : formElement;
-				form.reset();
 			},
 
-			
 			domReady() {
-
-				if ( $(".homePage").length )
-				fn.handleClearForm();
-
-
+				fn.hexlet()
 			}
 		}
 		
@@ -70,12 +38,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	
 		return fn;
 	
-	  })();
+	})();
 
 	
 
 });
 
 window.onload = function() {
-	console.log('Hi man! this window load')
+
 }
